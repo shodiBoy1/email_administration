67fc12bbf6d8c792c988e5e0c46233a7b3b24e3e
SHODIBOY <SOBIROVSH@YAHOO.COM>
2024-04-30 15:30:30 +0200

    first steps

---
+++ src/src/emailapp/Email.java
@@ -0,0 +1,22 @@
+package emailapp;
+
+public class Email {
+    private String firstname;
+   private String lastname;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // Constructor to receive the first name and last name
+
+    //Ask for the department
+
+    //Set the mailbox capacity
+
+    //Set the alternate email
+
+    //Change the password
+
+
+}
